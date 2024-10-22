63f409f0462d9a37373d35b708cb0a207c7aa3b1
Friedrich Große <friedrich@protectai.com>
2024-10-22 10:04:02 +0200

    Make it shiny ✨✨✨

--- main.go
+++ main.go
@@ -1,32 +1,93 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 	"runtime"
+	"strings"
 	"sync"
+	"time"
+
+	"github.com/charmbracelet/bubbles/progress"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/fgrosse/cli"
 )
 
-func main() {
-	numWorkers := flag.Int("workers", runtime.NumCPU(), "Number of workers to run")
-	flag.Parse()
+const (
+	padding  = 2
+	maxWidth = 80
+)
 
-	runWorkers(*numWorkers)
+var helpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#626262")).Render
+
+func main() {
+	n := flag.Int("workers", runtime.NumCPU(), "Number of workers to run")
+	flag.Parse()
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+	ctx := cli.Context()
+	m := NewModel(ctx, *n)
+
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		log.Fatalf("ERROR: %v", err)
+	}
+
 }
 
-func runWorkers(n int) {
+type Model struct {
+	numWorkers  int
+	progress    progress.Model
+	stopWorkers context.CancelFunc
+}
+
+type tickMsg time.Time
+
+func NewModel(ctx context.Context, numWorkers int) Model {
+	ctx, cancel := context.WithCancel(cli.Context())
+
+	m := Model{
+		progress: progress.New(
+			progress.WithDefaultGradient(),
+		),
+		numWorkers:  numWorkers,
+		stopWorkers: cancel,
+	}
+
+	go runWorkers(ctx, m.numWorkers)
+
+	return m
+}
+
+func (m Model) Init() tea.Cmd {
+	return tickCmd()
+}
+
+func runWorkers(ctx context.Context, n int) {
+	// log.Printf("Stressing CPU with %d workers", n)
 	var wg sync.WaitGroup
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go worker(&wg)
+		go worker(ctx, &wg)
 	}
 
 	wg.Wait()
 }
 
-func worker(wg *sync.WaitGroup) {
+func worker(ctx context.Context, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for {
-		fibonacci(35)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			fibonacci(35)
+		}
+
 	}
 }
 
@@ -40,4 +101,56 @@ func fibonacci(n int) int {
 		prev, curr = curr, prev+curr
 	}
 	return curr
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			m.stopWorkers()
+			return m, tea.Quit
+		default:
+			return m, nil
+		}
+
+	case tea.WindowSizeMsg:
+		m.progress.Width = msg.Width - padding*2 - 4
+		if m.progress.Width > maxWidth {
+			m.progress.Width = maxWidth
+		}
+		return m, nil
+
+	case tickMsg:
+		if m.progress.Percent() == 1.0 {
+			return m, nil
+		}
+
+		// Note that you can also use progress.Model.SetPercent to set the
+		// percentage value explicitly, too.
+		cmd := m.progress.IncrPercent(0.25)
+		return m, tea.Batch(tickCmd(), cmd)
+
+	// FrameMsg is sent when the progress bar wants to animate itself
+	case progress.FrameMsg:
+		progressModel, cmd := m.progress.Update(msg)
+		m.progress = progressModel.(progress.Model)
+		return m, cmd
+
+	default:
+		return m, nil
+	}
+}
+
+func (m Model) View() string {
+	pad := strings.Repeat(" ", padding)
+	return "\n Stressing CPUs" +
+		pad + m.progress.View() + "\n\n" +
+		pad + helpStyle(fmt.Sprintf("Using %d workers. Press q or ctrl+c to quit", m.numWorkers))
+}
+
+func tickCmd() tea.Cmd {
+	return tea.Tick(100*time.Millisecond, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
